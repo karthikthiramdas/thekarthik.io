7b954011c6ed412f8ea398a972234e86a5c7bdeb
karthikthiramdas <thekarthikthiramdas@gmail.com>
2026-08-09 12:28:08 +0530

    add static export for GitHub Pages

---
+++ _next/static/chunks/app/journal/page-b581ad157caa7514.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[170,246,405,732,822,916],{9039:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[356,619,441,255,358],()=>e(e.s=9039)),_N_E=e.O()}]);
